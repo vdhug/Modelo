18ae5c3410b2027053b768bc0b9cd1cfa89634b4
Vitor <vitorhug97@gmail.com>
2018-03-16 23:51:52 -0300

    Mudancas

--- Modelo/src/main/java/academico/entidades/Curso.java
+++ Modelo/src/main/java/academico/entidades/Curso.java
@@ -11,9 +11,7 @@ import javax.persistence.Id;
 @Entity
 public class Curso implements Serializable{
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
